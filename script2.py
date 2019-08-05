8b8b6413b2382240291f2d956ac0a222911a645f
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-08-05 18:17:16 -0500

    More runs

--- script2.py
+++ script2.py
@@ -73,7 +73,7 @@ df = df.drop('Date', axis=1)
 df.head()
 
 SEQ_LEN = 50
-FUTURE_PERIOD_PREDICT = 3
+FUTURE_PERIOD_PREDICT = 1
 RATIO_TO_PREDICT = "pct_change"
 EPOCHS = 10
 BATCH_SIZE = 64
@@ -202,3 +202,28 @@ print('Test accuracy:', score[1])
 model.save("models/{}".format(NAME))
 
 #tensorboard --logdir=logs
+def moving_test_window_preds(n_future_preds):
+
+    ''' n_future_preds - Represents the number of future predictions we want to make
+                         This coincides with the number of windows that we will move forward
+                         on the test data
+    '''
+    preds_moving = []                                    # Use this to store the prediction made on each test window
+    moving_test_window = [validation_x[0,:].tolist()]          # Creating the first test window
+    moving_test_window = np.array(moving_test_window)    # Making it an numpy array
+    
+    for i in range(n_future_preds):
+        preds_one_step = model.predict(moving_test_window) # Note that this is already a scaled prediction so no need to rescale this
+        preds_moving.append(preds_one_step[0,0]) # get the value from the numpy 2D array and append to predictions
+        preds_one_step = preds_one_step.reshape(1,1,1) # Reshaping the prediction to 3D array for concatenation with moving test window
+        moving_test_window = np.concatenate((moving_test_window[:,1:,:], preds_one_step), axis=1) # This is the new moving test window, where the first element from the window has been removed and the prediction  has been appended to the end
+        
+    preds_moving = scaler.inverse_transform(preds_moving)
+    
+    return preds_moving
+preds = model.predict(validation_x)
+#preds_moving = moving_test_window_preds(500)
+
+plt.plot(validation_x)
+#plt.plot(preds_moving)
+plt.show()
